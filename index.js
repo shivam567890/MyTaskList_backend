b93b6c189f806f69392665e04e87f655968c4d16
shivam567890 <shivam.kh007@gmail.com>
2023-05-27 03:00:03 +0530

    changes

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ app.use('/api/auth',require('./routes/auth'))
 app.use('/userpres',require('./routes/userpresence'))
 app.use('/api/notes',require('./routes/notes'))
 app.use('/otp',require('./routes/sendMail'))
-app.use('/',async(req,res)=>{
-  res.send("server is running");
-})
+// app.use('/',async(req,res)=>{
+//   res.send("server is running");
+// })
 app.listen(port, () => {
   console.log(`iNotebook backend  listening on port ${port}`)
 })
